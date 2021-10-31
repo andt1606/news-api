62dc852f51bb698eff280a1dd7572c3660729f09
andt1606 <anga0996@gmail.com>
2021-10-31 16:20:00 +0700

    init api

--- src/main/java/com/laptrinhjavaweb/api/NewAPI.java
+++ src/main/java/com/laptrinhjavaweb/api/NewAPI.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NewAPI {
     @GetMapping("/test")
-
     public String testAPI() {
 
         return "success";
